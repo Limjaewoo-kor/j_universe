242dac6326683dbbd8eea0c11c31c36da667009a
Ljw <ljw3944@naver.com>
2025-07-08 21:27:26 +0900

    1. oci 빌드 관련 커밋 추가수정

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+set -ex  # 오류 발생 시 종료, 명령 출력
 
-# 가상환경 또는 Python3 명시적으로 사용
 pip install -r requirements.txt
-
-# .env 파일이 존재하는 경우 환경 변수 적용 (옵션)
 export $(cat .env | xargs)
 
-# FastAPI 앱 실행 (main.py 기준으로)
 uvicorn main:app --host 0.0.0.0 --port 8000
