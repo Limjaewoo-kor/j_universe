2847637824fd5fb1a6135cc14317d3dbe50dc715
Ljw <ljw3944@naver.com>
2025-07-09 18:57:36 +0900

    1. oci 빌드 관련 커밋 추가수정

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-pip install -r requirements.txt
+pip install -r requirements.txt >> setup.log 2>&1
 export $(cat .env | xargs)
 
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 > uvicorn.log 2>&1 &
